9f65f155183c66fe25598a3630df4fab68ab6380
Neto test <stepan.utkin66@gmail.com>
2023-11-01 19:14:22 +0300

    NEWWW

--- Project1/Project1/FileName.cpp
+++ Project1/Project1/FileName.cpp
@@ -3,7 +3,9 @@
 
 class Figure {
 public:
-	Figure(int a) :Figure("Figure", a){
+	Figure(){
+		amount_of_edges = 0;
+		name = "Figure";
 	}
 	virtual void print_info() {
 		std::cout << name << ":" << std::endl;
@@ -13,24 +15,29 @@ public:
 			std::cout << "False" << std::endl;
 		std::cout << "The amount of edges:" << amount_of_edges << std::endl;
 	}
-	  virtual bool check(){
+	virtual bool check() {
 		if (amount_of_edges == 0)
 			return true;
 		else
 			return false;
 	}
 protected:
-	Figure(std::string name, int amount_of_edges) {
-		this->name = name;
-		this->amount_of_edges = amount_of_edges;
-	}
 	int amount_of_edges;
 	std::string name;
 };
 
-class Triangle:public Figure {
+class Triangle :public Figure {
 public:
-	Triangle(int amount):Triangle(10,10,20,60,40,70,amount,"Triangle") {}
+	Triangle(double a, double b, double c, double A, double B, double C){
+		this->a = a;
+		this->b = b;
+		this->c = c;
+		this->A = A;
+		this->B = B;
+		this->C = C;
+		this->name = "Triangle";
+		this->amount_of_edges = 3;
+	}
 	double get_a() {
 		return a;
 	}
@@ -51,36 +58,26 @@ public:
 	}
 
 	void print_info() override {
-	    Figure::print_info();
+		Figure::print_info();
 		std::cout << "Edges: " << "a = " << get_a() << " b = " << get_b() << " c = " << get_c() << std::endl;
-		std::cout << "Angles: " << "A = " << get_A() << " B = " << get_B() << " C = " << get_C() <<  std::endl;
+		std::cout << "Angles: " << "A = " << get_A() << " B = " << get_B() << " C = " << get_C() << std::endl;
 	}
 
-	  bool check() override{
-		if ((amount_of_edges == 3 )&&( A + B + C == 180))
+	bool check() override {
+		if ((amount_of_edges == 3) && (A + B + C == 180))
 			return true;
 		else
 			return false;
 	}
 protected:
-	Triangle(double a, double b, double c, double A, double B, double C, int amount,std::string name):Figure(name,amount) {
-		this->a = a;
-		this->b = b;
-		this->c = c;
-		this->A = A;
-		this->B = B;
-		this->C = C;
-	}
 	double a, b, c;
 	double A, B, C;
 };
 
 class isosceles_triangle : public Triangle {
-protected:
-	isosceles_triangle(double a, double b, double A, double B,std::string name,int amount) :Triangle(a, b, a, A, B, A,amount,name) {
-	}
 public:
-	isosceles_triangle(int amount) :isosceles_triangle(20, 10, 50, 80,"isosceles_triangle",amount) {
+	isosceles_triangle(double a, double b, double A, double B) :Triangle(a, b, a, A, B, A) {
+		this->name = "isosceles_triangle";
 	}
 	bool check() override {
 		if (Triangle::check() && A == C && a == c)
@@ -91,12 +88,11 @@ public:
 };
 
 class equilateral_triangle : public Triangle {
-protected:
-	equilateral_triangle(double a, double A,int amount,std::string name) :Triangle(a, a, a, A, A, A,amount,name) {
-	}
 public:
-	equilateral_triangle(int amount) :equilateral_triangle(25, 50,amount, "equilateral_triangle") {
+	equilateral_triangle(double a) :Triangle(a, a, a, 60, 60, 60) {
+		this->name = "equilateral_triangle";
 	}
+
 	bool check() override {
 		if (Triangle::check() && A == 60 && B == A && C == A && a == b && b == c)
 			return true;
@@ -106,14 +102,11 @@ public:
 };
 
 class right_triangle :public Triangle {
-protected:
-	right_triangle(double a,double b,double c, double A,double B, int amount, std::string name) :Triangle(a, b, c, A, B, 90, amount, name) {
-	}
 public:
-	right_triangle(int amount) :right_triangle(10,20,30,45,45, amount, "right_triangle") {
+	right_triangle(double a, double b, double c, double A, double B) :Triangle(a, b, c, A, B, 90) {
 	}
 	bool check() override {
-		if (Triangle::check() && C== 90)
+		if (Triangle::check() && C == 90)
 			return true;
 		else
 			return false;
@@ -123,7 +116,19 @@ public:
 
 class Quadrilateral :public Figure {
 public:
-	Quadrilateral(int amount) : Quadrilateral(10, 10, 20,30, 90, 90, 90,90, amount, "Quadrilateral") {}
+Quadrilateral(double a, double b, double c, double d, double A, double B, double C, double D){
+		this->a = a;
+		this->b = b;
+		this->c = c;
+		this->d = d;
+		this->A = A;
+		this->B = B;
+		this->C = C;
+		this->D = D;
+		this->name = "Quadrilateral";
+		this->amount_of_edges = 4;
+	}
+	
 	double get_a() {
 		return a;
 	}
@@ -151,38 +156,26 @@ public:
 
 	void print_info() override {
 		Figure::print_info();
-		std::cout << "Edges: " << "a = " << get_a() << " b = " << get_b() << " c = " << get_c()<<" d = " << get_d() << std::endl;
-		std::cout << "Angles: " << "A = " << get_A() << " B = " << get_B() << " C = " << get_C()<<" D = " << get_D() << std::endl;
+		std::cout << "Edges: " << "a = " << get_a() << " b = " << get_b() << " c = " << get_c() << " d = " << get_d() << std::endl;
+		std::cout << "Angles: " << "A = " << get_A() << " B = " << get_B() << " C = " << get_C() << " D = " << get_D() << std::endl;
 	}
 
 	bool check() override {
-		if ((amount_of_edges == 4) && (A + B + C + D== 360))
+		if ((amount_of_edges == 4) && (A + B + C + D == 360))
 			return true;
 		else
 			return false;
 	}
 protected:
-	Quadrilateral(double a, double b, double c,double d, double A, double B, double C,double D, int amount, std::string name) :Figure(name, amount) {
-		this->a = a;
-		this->b = b;
-		this->c = c;
-		this->d = d;
-		this->A = A;
-		this->B = B;
-		this->C = C;
-		this->D = D;
-	}
 	double a, b, c, d;
 	double A, B, C, D;
 };
 
+
 class parallelogram :public Quadrilateral {
-protected:
-	parallelogram(double a, double b, double A, double B,int amount, std::string name) :Quadrilateral(a, b, a, b, A, B, A, B, amount, name) {
-	}
-	
 public:
-	parallelogram(int amount) :parallelogram(10, 20, 150, 30,amount,"parallelogram") {
+	parallelogram(double a, double b, double A, double B) :Quadrilateral(a, b, a, b, A, B, A, B) {
+		this->name = "parallelogram ";
 	}
 	bool check() override {
 		if (Quadrilateral::check() && a == c && b == d && A == C && B == D)
@@ -194,11 +187,9 @@ public:
 
 
 class rhombus :public parallelogram {
-protected:
-	rhombus(double a, double A, double B, int amount, std::string name) :parallelogram(a,a, A, B, amount, name){
-	}
 public:
-	rhombus(int amount) :rhombus(10, 150, 30, amount, "rhombus") {
+	rhombus(double a, double A, double B) :parallelogram(a, a, A, B) {
+		this->name = "rhombus";
 	}
 	bool check() override {
 		if (parallelogram::check() && a == b)
@@ -209,12 +200,11 @@ public:
 };
 
 class square :public rhombus {
-protected:
-	square(double a, int amount, std::string name) :rhombus(a,90,90, amount, name) {}
 public:
-	square(int amount) :square(10, amount, "square") {
+	square(double a) :rhombus(a, 90, 90) {
+		this->name = "square";
 	}
-	bool check() override{
+	bool check() override {
 		if (rhombus::check() && A == B && A == 90)
 			return true;
 		else
@@ -223,11 +213,9 @@ public:
 };
 
 class rectangle : public parallelogram {
-protected:
-	rectangle(double a, double b, int amount, std::string name) :parallelogram(a, b, 90, 90, amount, name) {
-	}
 public:
-	rectangle(int amount) :rectangle(10, 20, amount, "rectangle") {
+	rectangle(double a, double b) :parallelogram(a, b, 90, 90) {
+		this->name = "rectangle";
 	}
 	bool check() override {
 		if (parallelogram::check() && A == B && A == 90)
@@ -238,46 +226,44 @@ public:
 };
 
 int main() {
-	Figure a(0);
+	Figure a;
 	a.print_info();
 	std::cout << std::endl;
 
-	Triangle t(3);
+	Triangle t(10, 20, 30, 40, 50, 60);
 	t.print_info();
 	std::cout << std::endl;
 
-	isosceles_triangle b(3);
+	isosceles_triangle b(20, 60, 70, 30);
 	b.print_info();
 	std::cout << std::endl;
 
-	equilateral_triangle e(3);
+	equilateral_triangle e(30);
 	e.print_info();
 	std::cout << std::endl;
 
-	right_triangle f(3);
+	right_triangle f(30, 40, 50, 50, 60);
 	f.print_info();
 	std::cout << std::endl;
 
-	Quadrilateral r(4);
+	Quadrilateral r(30, 40, 50, 60, 70, 80, 90, 100);
 	r.print_info();
 	std::cout << std::endl;
 
-	parallelogram p(5);
+	parallelogram p(30, 40, 120, 60);
 	p.print_info();
 	std::cout << std::endl;
 
-	rhombus u(4);
+	rhombus u(10, 20, 30);
 	u.print_info();
 	std::cout << std::endl;
 
-	square x(4);
+	square x(30);
 	x.print_info();
 	std::cout << std::endl;
 
-	rectangle y(4);
+	rectangle y(40, 50);
 	y.print_info();
 	std::cout << std::endl;
-
-	
 	return 0;
 }
